98f37646e5d20fbd894b854d9f5aa44ca9c904da
victora0007 <victor.saenz.bonilla@gmail.com>
2023-06-06 23:57:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ stremalit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Other you like')
  
