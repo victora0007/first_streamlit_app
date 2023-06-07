0ce46817075fbf1d50c6988b8c3d417a6fe34c0f
victora0007 <victor.saenz.bonilla@gmail.com>
2023-06-06 23:59:23 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit
 
 streamlit.title(' My Parents New Healthy Diner')
 
-stremalit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Other you like')
- 
