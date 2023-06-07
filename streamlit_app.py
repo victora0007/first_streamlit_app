399f984eda7074f038efd910ee55cd148ea128ed
victora0007 <victor.saenz.bonilla@gmail.com>
2023-06-06 23:55:26 -0500

    Update streamlit_app.py
    added header menu
    added text menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title(' My Parents New Healthy Diner')
+
+stremalit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
  
