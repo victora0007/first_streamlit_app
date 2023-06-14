972f2413c3d06a879504e9c3bbbc88baa42c62af
victora0007 <victor.saenz.bonilla@gmail.com>
2023-06-14 18:41:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
     
-  except URLError as e:
+except URLError as e:
     streamlit.error()
 
 # don´t run anything past here while we troubleshoot
