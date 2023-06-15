71161676e7338f26cfe0cc8085249cd822742bde
victora0007 <victor.saenz.bonilla@gmail.com>
2023-06-14 19:33:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
-#New Section to display fruityvice api response
+# New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   #fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
@@ -50,22 +50,31 @@ try:
 except URLError as e:
     streamlit.error()
 
+streamlit.header("The fruit load list contains:")
+# Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur
+        #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+        my_cur.execute("select * from fruit_load_list")
+        # or select * from pc_rivery_db.public.fruit_load_list
+        #my_data_row = my_cur.fetchone()
+        #my_data_rows = my_cur.fetchall()
+        #return my_cur.fetchone()
+        return my_cur.fetchall()
+    
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    #streamlit.text("Hello from Snowflake:")
+    #streamlit.text("The fruit load list contains:")
+    #streamlit.text(my_data_row)
+    #streamlit.dataframe(my_data_row)
+    streamlit.dataframe(my_data_rows)
+
+
 # don´t run anything past here while we troubleshoot
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-# or select * from pc_rivery_db.public.fruit_load_list
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text("The fruit load list contains:")
-streamlit.header("The fruit load list contains:")
-#streamlit.text(my_data_row)
-#streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
